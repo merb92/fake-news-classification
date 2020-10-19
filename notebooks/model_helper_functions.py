bd5a3ac0fa4ad2c896515a9f75420b16edeca51c
Mike Erb <merb92@gmail.com>
2020-10-19 11:19:39 -0400

    Add additional functions

--- notebooks/model_helper_functions.py
+++ notebooks/model_helper_functions.py
@@ -2,6 +2,8 @@ from sklearn.metrics import classification_report, plot_confusion_matrix
 import matplotlib.pyplot as plt
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+import re
+from nltk.tokenize import word_tokenize
 
 
 def passthrough(doc):
@@ -59,3 +61,43 @@ def lowercase_and_remove_stopwords(doc):
     """Remove stopwords and lowercase tokens"""
     stop_words = stopwords.words('english')
     return [token.lower() for token in doc if token.lower() not in stop_words]
+
+def lower_unless_all_caps(string_):
+    """
+    Make all words in the input string lowercase unless that
+    word is in all caps
+    """
+    words = string_.split()
+    processed_words = [w.lower() if not (w.isupper() and len(w) > 1) else w for w in words]
+    return ' '.join(processed_words)
+
+def remove_single_characters(word_list, exception_list):
+    """Remove all the single characters, except those on the exception list"""
+    return [w for w in word_list if (len(w) > 1 or w in exception_list)]
+
+def remove_words(word_list, words_to_remove):
+    """Remove all the words in the words_to_remove list from the words_list"""
+    return [w for w in word_list if w not in words_to_remove]
+
+def tokenize_and_normalize_title_and_text(title, text):
+    """Combine, tokenize, and normalize the title and text of a news story"""
+
+    URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    TWITTER_HANDLE_REGEX = r'(?<=^|(?<=[^\w]))(@\w{1,15})\b'
+    DATE_WORDS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+              'saturday', 'sunday', 'january', 'february', 'march', 'april',
+             'may', 'june', 'july', 'august', 'september', 'october',
+             'november', 'december']
+
+    title_text = ' '.join([title, text])
+    title_text = re.sub(URL_REGEX, '{link}', title_text)
+    title_text = re.sub(TWITTER_HANDLE_REGEX, '@twitter-handle', title_text)
+    title_text = lower_unless_all_caps(title_text)
+    title_text = re.sub(r'\d+', ' ', title_text)
+    title_text = re.sub(r'\(reuters\)', ' ', title_text)
+    tokens = word_tokenize(title_text)
+    tokens = remove_single_characters(tokens, ['i', '!'])
+    tokens = remove_words(tokens, ["'s"])
+    tokens = remove_words(tokens, DATE_WORDS)
+
+    return tokens
